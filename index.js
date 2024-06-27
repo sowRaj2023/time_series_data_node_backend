4a33b113e966653f6e95bed7b1fa8021e4003d32
sowRaj2023 <sownyuvan1180@gmail.com>
2024-06-27 07:39:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+const PATH = path.join(__dirname, "time_series_DB.db");
+const PORT = process.env.PORT || 1212;
+
+let db = null;
+
+const intializeDBAndServer = async () => {
+    try {
+        db = await open({
+            filename: PATH,
+            driver: sqlite3.Database
+        }) 
+        app.listen(PORT, () => console.log(`running http://localhost:${PORT}`));
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+}
+
+intializeDBAndServer();
+
+
+app.get("/api/time-series-data", async (req, res) => {
+    const {period = "daily"} = req.query;   
+    let SQL_QUERY = `SELECT strftime("%d-%m-%Y", timestamp) as day, AVG(value) as value from time_series_data
+            GROUP BY strftime("%d-%m-%Y", timestamp);`
+
+    if (period === "weekly") {
+        SQL_QUERY = `SELECT strftime("%W-%Y", timestamp) as week of year, AVG(value) as value from time_series_data
+            GROUP BY strftime("%W", timestamp);`
+    }
+    if (period === "monthly") {
+        SQL_QUERY = `SELECT strftime("%m-%Y", timestamp) as month of year, AVG(value) as value from time_series_data
+            GROUP BY strftime("%m-%Y", timestamp);`
+    }
+    const dbResponse = await db.all(SQL_QUERY);
+    res.send(dbResponse)
+})
